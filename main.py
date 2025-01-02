ab40ec0e59e913ac015b36b994c1e16a28313a1a
Stefan Rietzinger <e11910536@student.tuwien.ac.at>
2025-01-02 15:02:10 +0100

    test commit

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ if __name__=="__main__":
     # download latest version of dataset!
     path = kagglehub.dataset_download("kmader/skin-cancer-mnist-ham10000")
     print("Path to dataset files:", path)
+
+    print("moin moin!")

b8069b834f1a0a4a2034aca439f5b3cd0fb9036b
Stefan Rietzinger <e11910536@student.tuwien.ac.at>
2025-01-02 18:53:45 +0100

    added testspace

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import lib.lib as lib
 if __name__=="__main__":
 
     # download latest version of dataset!
-    path = kagglehub.dataset_download("kmader/skin-cancer-mnist-ham10000")
-    print("Path to dataset files:", path)
-
-    print("moin moin!")
+    # path = kagglehub.dataset_download("kmader/skin-cancer-mnist-ham10000")
+    # print("Path to dataset files:", path)
+    # print("moin moin!")
+    ...
